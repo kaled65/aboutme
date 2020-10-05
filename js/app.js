e29133920b9e0f01c747da2bdf99301ea068ca70
kaled65 <kaledzarifeh65@gmail.com>
2020-10-05 16:32:03 +0300

    adding

---
+++ js/app.js
@@ -0,0 +1,46 @@
+'use strict'
+var age = prompt('Am I a millennial? y/n');
+console.log(age)
+if (age.toLowerCase() == 'y'){
+alert ('you are right');
+}
+else {
+    alert('you are wrong');
+}
+
+var sport = prompt('do I practice sport? y/n');
+console.log(sport)
+if (sport.toLowerCase() == 'y'){
+alert ('you are right');
+}
+else {
+    alert('you are wrong');
+}
+
+var math = prompt('Am I good at math? y/n');
+console.log(math)
+if (math.toLowerCase() == 'n'){
+alert ('you are right');
+}
+else {
+    alert('you are wrong');
+}
+
+var food = prompt('Do I like spicy food? y/n');
+console.log(food)
+if (food.toLowerCase() == 'n'){
+alert ('you are right');
+}
+else {
+    alert('you are wrong');
+}
+
+
+var lan = prompt('Do I speak French? y/n');
+console.log(lan)
+if (lan.toLowerCase() == 'n'){
+alert ('you are right');
+}
+else {
+    alert('you are wrong');
+}
